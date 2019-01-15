fc1f7c4b5e23425b6afee6946ea1cb267b3c2152
kumarro <rohit.kumar@cdk.com>
2019-01-15 14:23:25 +0530

    publish-site navigation

---
+++ js/main.js
@@ -0,0 +1,62 @@
+(function () {
+	var header = document.getElementById("mainHeader");
+
+	function changeHeader() {
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+	}
+
+	var didScroll = false;
+
+	$(window).scroll(function () {
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			didScroll = false;
+			changeHeader();
+		}
+	}, 100);
+
+	changeHeader();
+
+	document.getElementById("open-nav").addEventListener("click", function (event) {
+		event.preventDefault();
+		document.body.classList.toggle("nav-open");
+		changeHeader();
+	});
+
+	$("a[href*=\\#]").on("click", function (event) {
+		event.preventDefault();
+
+		$("html, body").animate({
+			scrollTop: $(this.hash).offset().top
+		}, 500);
+	});
+
+    /*Scroll to top when arrow up clicked BEGIN*/
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $("#back2Top").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "fast");
+            return false;
+        });
+
+	});
+
+    $(document).ready(function() {
+        var images = ['face-1.jpg', 'face-2.jpg', 'face-3.jpg', 'face-4.jpg', 'face-5.jpg', 'face-6.jpg', 'face-7.jpg'];
+
+        $('.random-images').css({'background-image': 'url(/images/random/' + Math.floor(Math.random() * 13) + '.webp)'});
+    });
+})();
+
